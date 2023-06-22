b4c66e866efc8e93e9a8877ddd4e94b290cc25fe
Sergei Klimov <s.klimov@bk.ru>
2023-06-22 12:37:31 +0300

    server.py refactored

--- server.py
+++ server.py
@@ -1,32 +1,51 @@
 import json
 
 import trio
+import trio.testing
 from trio_websocket import serve_websocket, ConnectionClosed
+
+
+send_channel, receive_channel = trio.open_memory_channel(0)
+buses = dict()
 
 
 async def send_message(request):
     ws = await request.accept()
 
-    while True:
-        buses = {
-            'msgType': 'Buses',
-            'buses': [],
-        }
-        try:
-            message = await ws.get_message()
-            coordinate = json.loads(message)
-            if coordinate.get('msgType') == 'newBounds':
-                continue
+    async for message in receive_channel:
+        buses_msg = json.dumps(
+            {'msgType': 'Buses', 'buses': json.loads(message)}
+        )
 
-            print('Received message: %s' % message)
-            buses['buses'].append(coordinate)
-            await ws.send_message(json.dumps(buses))
+        try:
+            await ws.send_message(buses_msg)
         except ConnectionClosed:
             break
 
 
+async def get_message(request):
+    ws = await request.accept()
+
+    try:
+        while message := await ws.get_message():
+            buse = json.loads(message)
+            buses[buse['busId']] = buse
+
+            await send_channel.send(
+                json.dumps(list(buses.values()), ensure_ascii=False)
+            )
+    except ConnectionClosed:
+        pass
+
+
 async def main():
-    await serve_websocket(send_message, '127.0.0.1', 8000, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(
+            serve_websocket, get_message, '127.0.0.1', 8080, None
+        )
+        nursery.start_soon(
+            serve_websocket, send_message, '127.0.0.1', 8000, None
+        )
 
 
 if __name__ == '__main__':
