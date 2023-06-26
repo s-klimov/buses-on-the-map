70e0de2d30013602ea039e2723a42ef76e98e989
Sergei Klimov <s.klimov@bk.ru>
2023-06-26 22:23:02 +0300

    relaunch decorator added

--- fake_bus.py
+++ fake_bus.py
@@ -4,25 +4,34 @@ import json
 import logging
 import os
 from collections import deque
+from functools import wraps
 from pathlib import Path
 from random import randrange
 
 import asyncclick as click
 import asyncstdlib as a
 import trio
+import trio_websocket
 from trio import MemoryReceiveChannel, MemorySendChannel
 from trio_websocket import open_websocket_url
 
 
 ROUTES_DIR = 'routes'    # папка с маршрутами автобусов
 BUS_NUM_LENGTH = 3       # количество символов в номере автобуса
+RELAUNCH_INTERVAL = (
+    1  # интервал переподключения в секундах при обрыве соединения с сервером
+)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S',
+)
 logger = logging.getLogger('fake-bus')
+
 
 async def load_routes(directory_path=ROUTES_DIR):
     for filename in os.listdir(directory_path):
-        logger.info("Открываем файл %s" % (filename, ))
+        logger.info('Открываем файл %s' % (filename,))
         if filename.endswith('.json'):
             filepath = os.path.join(Path(directory_path), filename)
             async with await trio.open_file(
@@ -33,7 +42,12 @@ async def load_routes(directory_path=ROUTES_DIR):
 
 
 async def run_bus(
-    send_channel: MemorySendChannel, bus_id: str, points: list, route_name: str, refresh_timeout: float,/
+    send_channel: MemorySendChannel,
+    bus_id: str,
+    points: list,
+    route_name: str,
+    refresh_timeout: float,
+    /,
 ):
     """
 
@@ -58,8 +72,32 @@ def generate_bus_id(route_id, bus_index, emulator_id):
     return f'{route_id}-{emulator_id}{str(bus_index).zfill(BUS_NUM_LENGTH)}'
 
 
+def relaunch_on_disconnect(f):
+    @wraps(f)
+    async def wrapper(*args, **kwds):
+        with suppress(KeyboardInterrupt):
+            while True:
+                try:
+                    await f(*args, **kwds)
+                except (
+                    trio_websocket.HandshakeError,
+                    trio_websocket.ConnectionClosed,
+                ):
+                    logger.error(
+                        'Ошибка соединения с сервером. Попытка подключения через %d сек'
+                        % (RELAUNCH_INTERVAL,)
+                    )
+                    await trio.sleep(RELAUNCH_INTERVAL)
+
+    return wrapper
+
+
+@relaunch_on_disconnect
 async def send_updates(
-    server: str, websockets_number: int, receive_channel: MemoryReceiveChannel, /
+    server: str,
+    websockets_number: int,
+    receive_channel: MemoryReceiveChannel,
+    /,
 ):
     """
     Отправляет координаты автобуса по web-сокету. Web-сокет выбирается случайным образом из заданных.
@@ -72,25 +110,69 @@ async def send_updates(
             await stack.enter_async_context(open_websocket_url(server))
             for _ in range(websockets_number)
         ]
+        logger.info('Открыто %d сокетов.' % (len(sockets),))
         async for message in receive_channel:
             await sockets[randrange(websockets_number)].send_message(message)
 
 
 def validate_routes_number(ctx, param, value):
     if value < 1 or value > 595:
-        raise click.BadParameter("Количество маршрутов должно быть от 1 до 595.")
+        raise click.BadParameter(
+            'Количество маршрутов должно быть от 1 до 595.'
+        )
     return value
 
 
 @click.command()
-@click.option("--server", default='ws://127.0.0.1:8080/ws', show_default=True, help="Адрес сервера.")
-@click.option("--routes_number", default=595, show_default=True, callback=validate_routes_number, help="Количество маршрутов (от 1 до 595).")
-@click.option("--buses_per_route", default=100, show_default=True, help="Количество автобусов на каждом маршруте.")
-@click.option("--websockets_number", default=10, show_default=True, help="Количество открытых веб-сокетов.")
-@click.option("--emulator_id", default="", help="Префикс к busId на случай запуска нескольких экземпляров имитатора.")
-@click.option("--refresh_timeout", type=float, default=0.1, show_default=True, help="Задержка в обновлении координат сервера.")
-@click.option('-v', '--verbose', count=True, help="Настройка логирования.")  # https://click.palletsprojects.com/en/8.1.x/options/#counting
-async def main(server, routes_number, buses_per_route, websockets_number, emulator_id, refresh_timeout, verbose):
+@click.option(
+    '--server',
+    default='ws://127.0.0.1:8080/ws',
+    show_default=True,
+    help='Адрес сервера.',
+)
+@click.option(
+    '--routes_number',
+    default=595,
+    show_default=True,
+    callback=validate_routes_number,
+    help='Количество маршрутов (от 1 до 595).',
+)
+@click.option(
+    '--buses_per_route',
+    default=100,
+    show_default=True,
+    help='Количество автобусов на каждом маршруте.',
+)
+@click.option(
+    '--websockets_number',
+    default=10,
+    show_default=True,
+    help='Количество открытых веб-сокетов.',
+)
+@click.option(
+    '--emulator_id',
+    default='',
+    help='Префикс к busId на случай запуска нескольких экземпляров имитатора.',
+)
+@click.option(
+    '--refresh_timeout',
+    type=float,
+    default=0.1,
+    show_default=True,
+    help='Задержка в обновлении координат сервера.',
+)
+@click.option(
+    '-v', '--verbose', count=True, help='Настройка логирования.'
+)  # https://click.palletsprojects.com/en/8.1.x/options/#counting
+async def main(
+    server,
+    routes_number,
+    buses_per_route,
+    websockets_number,
+    emulator_id,
+    refresh_timeout,
+    verbose,
+):
 
     if verbose in [0, 1]:
         level = logging.ERROR
@@ -106,7 +188,9 @@ async def main(server, routes_number, buses_per_route, websockets_number, emulat
     send_channel, receive_channel = trio.open_memory_channel(0)
 
     async with trio.open_nursery() as nursery:
-        nursery.start_soon(send_updates, server, websockets_number, receive_channel)
+        nursery.start_soon(
+            send_updates, server, websockets_number, receive_channel
+        )
 
         async for i, route in a.enumerate(load_routes()):
             if i == routes_number:
@@ -119,7 +203,13 @@ async def main(server, routes_number, buses_per_route, websockets_number, emulat
             points = deque(coordinates)
 
             for bus_index in range(randrange(1, buses_per_route)):
-                logger.debug("Запускаем автобус %s по маршруту %s" % (bus_index, route['name'],))
+                logger.debug(
+                    'Запускаем автобус %s по маршруту %s'
+                    % (
+                        bus_index,
+                        route['name'],
+                    )
+                )
 
                 points.rotate(
                     randrange(len(coordinates))
@@ -137,4 +227,4 @@ async def main(server, routes_number, buses_per_route, websockets_number, emulat
 
 if __name__ == '__main__':
     with suppress(KeyboardInterrupt):
-        trio.run(main(_anyio_backend="trio"))
+        trio.run(main(_anyio_backend='trio'))
