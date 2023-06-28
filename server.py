e6898c7598b757ef322b524248ed0542ff2bf765
Sergei Klimov <s.klimov@bk.ru>
2023-06-28 17:14:12 +0300

    listen_browser function added

--- server.py
+++ server.py
@@ -1,17 +1,41 @@
 import json
+import logging
+import warnings
 from contextlib import suppress
 
 import trio
 import trio.testing
+import asyncclick as click
+from trio import TrioDeprecationWarning
 from trio_websocket import serve_websocket, ConnectionClosed
 
 
+warnings.filterwarnings(action='ignore', category=TrioDeprecationWarning)
 send_channel, receive_channel = trio.open_memory_channel(0)
 buses = dict()
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s: %(name)s: %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S',
+)
+logger = logging.getLogger('server')
 
 
-async def send_message(request):
+async def exchange_messages(request):
     ws = await request.accept()
+
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(listen_browser, ws)
+        nursery.start_soon(send_buses, ws)
+
+
+async def listen_browser(ws):
+
+    with suppress(ConnectionClosed):
+        while message := await ws.get_message():
+            logger.debug('%s', (message,))
+
+
+async def send_buses(ws):
 
     async for message in receive_channel:
         buse = json.loads(message)
@@ -20,7 +44,7 @@ async def send_message(request):
         buses_msg = json.dumps(
             {'msgType': 'Buses', 'buses': list(buses.values())}
         )
-
+        logger.debug('%s', (buses_msg,))
         try:
             await ws.send_message(buses_msg)
         except ConnectionClosed:
@@ -30,24 +54,38 @@ async def send_message(request):
 async def get_message(request):
     ws = await request.accept()
 
-    try:
+    with suppress(ConnectionClosed):
         while message := await ws.get_message():
 
             await send_channel.send(message)
-    except ConnectionClosed:
-        pass
 
 
-async def main():
+@click.command()
+@click.option(
+    '-v', '--verbose', count=True, help='Настройка логирования.'
+)  # https://click.palletsprojects.com/en/8.1.x/options/#counting
+async def main(verbose):
+
+    if verbose in [0, 1]:
+        level = logging.ERROR
+    elif verbose == 2:
+        level = logging.WARNING
+    elif verbose == 3:
+        level = logging.INFO
+    else:
+        level = logging.DEBUG
+
+    logger.setLevel(level)
     async with trio.open_nursery() as nursery:
         nursery.start_soon(
             serve_websocket, get_message, '127.0.0.1', 8080, None
         )
         nursery.start_soon(
-            serve_websocket, send_message, '127.0.0.1', 8000, None
+            serve_websocket, exchange_messages, '127.0.0.1', 8000, None
         )
 
 
 if __name__ == '__main__':
+
     with suppress(KeyboardInterrupt):
-        trio.run(main)
+        trio.run(main(_anyio_backend='trio'))
